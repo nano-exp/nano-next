5ab5daa3a0bde4905f746debeee41a0ed672bf23
cbdyzj <cbdyzj@jianzhao.org>
2020-01-07 10:38:37 +0800

    Amend

--- pages/api/hi.js
+++
@@ -1,9 +0,0 @@
-import wiki from '../../utils/wiki_extracts'
-
-export default async (req, res) => {
-    if (req.query['wiki']) {
-        res.send(await wiki(req.query['wiki']))
-        return
-    }
-    res.send('hi')
-}
