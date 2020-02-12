afe4c6f68b07f613e1cfbcad33f1c557e6e38594
cbdyzj <cbdyzj@jianzhao.org>
2020-02-12 16:57:12 +0800

    detail: amend

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const cacheStorageKey = 'nano-pwa'
+
+const cacheList = [
+    '/',
+    'pwa.html',
+    "icon.png"
+]
+
+self.addEventListener('install', ev => {
+    ev.waitUntil(
+        caches.open(cacheStorageKey)
+            .then(cache => cache.addAll(cacheList))
+            .then(() => self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function(ev) {
+    ev.respondWith(
+        caches.match(ev.request).then(function(response) {
+            if (response != null) {
+                return response
+            }
+            return fetch(ev.request.url)
+        })
+    )
+})
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        Promise.all(
+            caches.keys().then(cacheNames => {
+                return cacheNames.map(name => {
+                    if (name !== cacheStorageKey) {
+                        return caches.delete(name)
+                    }
+                })
+            })
+        ).then(() => {
+            return self.clients.claim()
+        })
+    )
+})
